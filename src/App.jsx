b0b20f47f6da5fe1d38f7c06061224690b6432d1
sanjay-in <sanjaysridharan@outlook.com>
2024-09-10 18:09:41 +0530

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Home from "./components/Home/Home";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
