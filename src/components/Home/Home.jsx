c4fbf78e4c7e08723cb631a2171d4333228d23b4
sanjay-in <sanjaysridharan@outlook.com>
2024-09-10 22:44:23 +0530

    modify home ui

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Home.css";
 import ConnectMetaMask from "../ConnectMetamask/ConnectMetaMask";
+import EnterLottery from "../EnterLottery/EnterLottery";
+import confetti from "../../assets/confetti.png";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="home-header">Decentralized Lottery</h1>
-      <ConnectMetaMask />
+      <h1 className="home-header">
+        <span>Decentralized</span>Lottery
+      </h1>
+      {/* <ConnectMetaMask /> */}
+      <EnterLottery />
+      <img className="confetti-background" src={confetti} />
     </div>
   );
 };
