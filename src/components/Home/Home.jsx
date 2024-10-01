3bed37c9a2ebd058a05f51550b2a6d83fee92073
sanjay-in <sanjaysridharan@outlook.com>
2024-10-01 11:36:07 +0530

    Add connect metamask logic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,48 @@
-import React from "react";
-import "./Home.css";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import ConnectMetaMask from "../ConnectMetamask/ConnectMetaMask";
 import EnterLottery from "../EnterLottery/EnterLottery";
 import confetti from "../../assets/confetti.png";
+import "./Home.css";
 
 const Home = () => {
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  const connectToMetamask = async () => {
+    if (window.ethereum) {
+      await window.ethereum
+        ?.request({
+          method: "eth_requestAccounts",
+        })
+        .then(() => {
+          setIsWalletConnected(true);
+        })
+        .catch((error) => console.log(error));
+    } else {
+      toast.error("Metamask not detected", {
+        position: "top-center",
+        hideProgressBar: "true",
+        autoClose: 3000,
+        closeOnClick: true,
+        pauseOnHover: true,
+      });
+      setIsWalletConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    connectToMetamask();
+  }, []);
+
   return (
     <div>
       <h1 className="home-header">
         <span>Decentralized</span>Lottery
       </h1>
-      {/* <ConnectMetaMask /> */}
-      <EnterLottery />
-      <img className="confetti-background" src={confetti} />
+      {isWalletConnected ? <EnterLottery /> : <ConnectMetaMask connectToMetamask={connectToMetamask} />}
+      <img className="confetti-background-1" src={confetti} />
+      <img className="confetti-background-2" src={confetti} />
+      <ToastContainer />
     </div>
   );
 };
