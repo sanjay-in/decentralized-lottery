bda252dad12963c95f25d1d79b47c66e62af2da3
sanjay-in <sanjaysridharan@outlook.com>
2024-10-01 11:54:48 +0530

    update variables

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,8 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("dotenv").config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const PRIVATE_KEY = [process.env.PRIVATE_KEY] || [];
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -16,17 +19,19 @@ module.exports = {
     localhost: {
       chainId: 31337,
     },
-    // sepolia: {
-    //   chainId: 11155111,
-    //   url: SEPOLIA_RPC_URL,
-    //   accounts: PRIVATE_KEY,
-    // },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY,
+      blockConfirmation: 6,
+      saveDeployments: true,
+    },
   },
-  // etherscan: {
-  //   apiKey: {
-  //     sepolia: ETHERSCAN_API_KEY,
-  //   },
-  // },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
   namedAccounts: {
     deployer: {
       default: 0,
