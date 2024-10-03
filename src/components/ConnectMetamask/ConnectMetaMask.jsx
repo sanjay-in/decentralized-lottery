9c19aae3d5b1513c4af6178c544da0a22108555d
sanjay-in <sanjaysridharan@outlook.com>
2024-10-03 22:30:48 +0530

    create connect metamask btn

--- src/components/ConnectMetamask/ConnectMetaMask.jsx
+++ src/components/ConnectMetamask/ConnectMetaMask.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "./ConnectMetaMask.css";
 
-const ConnectMetaMask = () => {
+const ConnectMetaMask = ({ connectToMetamask }) => {
   return (
     <div className="connect-metamask">
-      <p className="connect-metamask-text">You're MetaMask wallet is not connected</p>
-      <Button className="connect-metamask-btn">Connect to MetaMask</Button>
+      <p className="connect-metamask-text">Your MetaMask wallet is not connected</p>
+      <Button className="connect-metamask-btn" variant="warning" onClick={connectToMetamask}>
+        Connect to MetaMask
+      </Button>
     </div>
   );
 };
